a92faae738febe47acede2d1b9bb19767a492f13
Daniel Ors <do7811@gmail.com>
2019-01-23 00:41:34 -0500

    continued consolidation and fleshed out new handler functions

--- main.js
+++ main.js
@@ -27,15 +27,8 @@ const QandA =[
 
 const STORE = {
   currentQuestion: 0,
-  currentPage: [],
-  score: [ 0 ],
+  score: 0,
   wrongAnswer: [],
-  view: {
-    start: {},
-    question: {},
-    answer: {},
-    result: {}
-  }
 };
 
 
@@ -49,68 +42,90 @@ const IMAGES = {
     { url: 'https://drive.google.com/file/d/1woS9_oqdLsv57Ugy930Jup5lSq92ooGP/view?usp=sharing' , alt: 'this plate is broken on the floor because of you. how dare you.' } ]
 };
 
-function start() {
+// handling start page
+
+function startView() {
+  $(document).ready( function () {
+    console.log('start document ready ran');
+    $('.js-master-container').html(`<div class="start-title">
+    <h1>How well do you know US Law?</h1>
+  </div>
+  <div class="start-button-container">
+      <input type="button" value="START" class="start-button js-start-button"/>
+  </div>`);
+  });
+}
+
+// handle start button
+
+function startButton() {
+  console.log('startButton ran');
   $('.start-button-container').on('click', 'js-start-button', function() {
     console.log('start button pressed - ENGAGE!');
-    let i = STORE.currentQuestion;
-    $('.js-master-container').html(`<div class="question-title">
-    <h1>QUESTION:</h1>
-    <h2></h2>
-  </div>
-  <form class="question-answers js-question-answers">
-  <input type="radio" name="question" value=${QandA[i].answers[0]} required> ${QandA[i].answers[0]}<br>
-    <input type="radio" name="question" value=${QandA[i].answers[1]} required> ${QandA[i].answers[1]} <br>
-    <input type="radio" name="question" value=${QandA[i].answers[2]} required> ${QandA[i].answers[2]}<br>
-    <input type="radio" name="question" value=${QandA[i].answers[3]} required> ${QandA[i].answers[3]} <br>
-    <input type="submit" name="Submit" value="Submit"/>
-    </form>`);
+    $(event).preventDefault();
+    questionView();
   });
 }
 
 // template generator for answer lists
 
-function generateAnswerList() {
-  // call on STORE for the current question number, then generate the
-  // HTML list of answers accordingly
-  let i = STORE.currentQuestion;
-  console.log('event listener works inside answerList');
-  $('.js-question-answers').html(`<input type="radio" name="question" value=${QandA[i].answers[0]} required> ${QandA[i].answers[0]}<br>
-    <input type="radio" name="question" value=${QandA[i].answers[1]} required> ${QandA[i].answers[1]} <br>
-    <input type="radio" name="question" value=${QandA[i].answers[2]} required> ${QandA[i].answers[2]}<br>
-    <input type="radio" name="question" value=${QandA[i].answers[3]} required> ${QandA[i].answers[3]} <br>
-    <input type="submit" name="Submit" value="Submit"/>`);
- 
-  console.log('after event listener');
+// function generateAnswerList() {
+//   // call on STORE for the current question number, then generate the
+//   // HTML list of answers accordingly
+//   let i = STORE.currentQuestion;
+//   $('.js-question-answers').html(`<input type="radio" name="question" value=${QandA[i].answers[0]} required> ${QandA[i].answers[0]}<br>
+//     <input type="radio" name="question" value=${QandA[i].answers[1]} required> ${QandA[i].answers[1]} <br>
+//     <input type="radio" name="question" value=${QandA[i].answers[2]} required> ${QandA[i].answers[2]}<br>
+//     <input type="radio" name="question" value=${QandA[i].answers[3]} required> ${QandA[i].answers[3]} <br>
+//     <input type="submit" name="Submit" value="Submit"/>`);
+   
+// }
   
-}
-  
-function renderQuestionText() {
-  // call on STORE.currentQuestion and generate the HTML for the correct
-  // question prompt from QUESTION storage
-  let i = STORE.currentQuestion;
-  $('.question-title').html(`<h1>QUESTION:</h1> <h2> ${QandA[i].question} </h2>`);
-  console.log('renderQuestionText');
+// function renderQuestionText() {
+//   // call on STORE.currentQuestion and generate the HTML for the correct
+//   // question prompt from QUESTION storage
+//   let i = STORE.currentQuestion;
+//   $('.question-title').html(`<h1>QUESTION:</h1> <h2> ${QandA[i].question} </h2>`);
+//   console.log('renderQuestionText');
 
+// }
+
+// handling question page
+
+function questionView() {
+// here we will pass in all html inside master-container for each question to be rendered
+  let i = STORE.currentQuestion;
+  $('.js-master-container').html(`<div class="question-title">
+  <h1>QUESTION:</h1>
+  <h2>${QandA[i].question}</h2>
+  </div>
+  <form class="question-answers js-question-answers">
+  <input type="radio" name="question" value=${QandA[i].answers[0]} required> ${QandA[i].answers[0]}<br>
+  <input type="radio" name="question" value=${QandA[i].answers[1]} required> ${QandA[i].answers[1]} <br>
+  <input type="radio" name="question" value=${QandA[i].answers[2]} required> ${QandA[i].answers[2]}<br>
+  <input type="radio" name="question" value=${QandA[i].answers[3]} required> ${QandA[i].answers[3]} <br>
+  <input type="submit" name="Submit" value="Submit"/>
+  </form>
+  <div class="tracker-container">
+    <p class="tracker js-tracker">Question ${STORE.currentQuestion} of 10  |  ${STORE.score} of 10 Correct</p>
+  </div>`);
+  STORE.currentQuestion++;
 }
 
 function handleAnswerSubmitted() {
   // listen for the answer submitted by the user, then check against STORE
   // to see if it is the correct answer, then send user to appropriate
-  // answer result page (CORRECT/WRONG)
+  // answer result view (CORRECT/WRONG)
   $('.js-question-answers').on('submit', function(event){
     console.log('answersubmitted is running');
     event.preventDefault();
-    //debugger;
     let answer = $('input[name=question]:checked').val();
 
     if( answer === QandA[STORE.currentQuestion].correctAnswer){
-      //send to correct.html page
-      //   window.location.href="correct.html"
       console.log('clicked correct answer');
       answerCorrect();
     }
     else {
-      // window.location.href="wrong.html"
       STORE.wrongAnswer.push(answer);
       console.log('clicked wrong answer');
       answerWrong();
@@ -122,20 +137,34 @@ function answerWrong(){
   // $(document).ready(function(){
   console.log('the wrong answer text');
   $('.fail-text').html(`<h2>Aww, you got the answer wrong.</h2><br>
-    <p>You answered ${STORE.wrongAnswer[STORE.wrongAnswer.length -1]}, but the correct answer is ${QandA[STORE.currentQuestion].correctAnswer} </p>`);
-  //})
-
+    <p>You answered ${STORE.wrongAnswer[STORE.wrongAnswer.length -1]}, 
+    but the correct answer is ${QandA[STORE.currentQuestion].correctAnswer} 
+    </p>`);
+  nextQuestion();
 }
 
 function answerCorrect(){
+  STORE.score++;
+  nextQuestion();
+}
 
+// handling next question rendering on next question button click
+function nextQuestion() {
+  if ( STORE.currentQuestion < QandA.length ) {
+    $('js-next-question-container').on( 'click', '.js-next-question-button', function () {
+      STORE.currentQuestion++;
+      questionView();
+    });
+  }
+  else {
+    handleResults();
+  }
 }
 
 // handle score memory
 function handleScore() {
   // listen for answer correct/false, then push new count of correct answers to
   // STORE.score for memory
-
 
 }
 
@@ -144,7 +173,7 @@ function handleTracker(){
 }
 
 function renderQuizApp(){
-  //render the quiz app
+  //render the quiz app elements
   
   
   handleAnswerSubmitted();
@@ -152,17 +181,32 @@ function renderQuizApp(){
 
 function handleResults(){
 // check handleScore to see if user passes or fails
-// send to correct results page
+// send to correct results view
+// need to build template for results within master-container here
+}
+
+// handle restart quiz
+
+function restartQuiz() {
+  // listen for restart button press to reinitialize start view
+  // and clear all user data in STORE
+  $('.js-restart').on( 'click', function () {STORE.currentQuestion = 0;
+    STORE.score = 0;
+    STORE.wrongAnswer = [];});
+  startView();
 }
 
 
 function generateQuizApp(){
-  start();
+  startView();
+  startButton();
+  questionView();
   handleAnswerSubmitted();
   handleScore();
   
-  renderQuestionText();
-  generateAnswerList();
+  // renderQuestionText();
+  // generateAnswerList();
+
   handleResults();
   handleTracker();
   
